78f21a3731adfa3e72a56c39d86a97ad860e66bb
Florentin Wujciak <florentin_wujciak@ens.univ-artois.fr>
2023-12-17 12:15:08 +0100

    First draft

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,44 @@
 import Image from 'next/image'
-import styles from './page.module.css'
+import styles from  './ui/home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <body className={styles.body}>
+      <div className={styles.background}>
+          <div className={styles.content}>
+            
+            <Image src="/pp.png" alt="Photo de profil" width={150} height={150}  style={{position: 'absolute',
+              left: '50%',
+              bottom: '0%',
+              top: '0%',
+              transform: 'translate(-50%, -50%)',
+              zIndex: '3',}}>
+            
+            </Image>
+            
+            <h1 className={styles.title}>Wujciak Florentin</h1>
+            <h2 className={styles.subtitle}>Junior App Developer</h2>
+            
+            <div className={styles.divider}></div>
+            
+            <div className={styles.iconBox}>
+              <div className={styles.iconItem}>
+                <span className={styles.icon}>&#9990;</span>
+                <p className={styles.iconText}> +33641327100</p>
+              </div>
+              <div className={styles.iconItem}>
+                <span className={styles.icon}>&#9993;</span>
+                <p className={styles.iconText}> wujciak.florentin.pro@gmail.com</p>
+              </div>
+              <div className={styles.iconItem}>
+                <svg className={styles.iconGlobe} data-testid="geist-icon" fill="none" height="24" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="24"><circle cx="12" cy="12" r="10"/><path d="M2 12h20"/><path d="M12 2a15.3 15.3 0 014 10 15.3 15.3 0 01-4 10 15.3 15.3 0 01-4-10 15.3 15.3 0 014-10z"/></svg>
+                <a target="_blank" title="Découvrir mes projets sur Github" href="https://github.com/Florentin-Wujciak?tab=repositories" className={styles.iconLink}>My Github repositories</a>
+              </div>
+            </div>
+          
+          </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </body>
   )
 }
