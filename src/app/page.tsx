4dd29f2f39b526f0b5e43b190a874227b63bc5d5
Florentin Wujciak <florentin_wujciak@ens.univ-artois.fr>
2023-12-21 17:16:04 +0100

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,18 +9,21 @@ export default function Home() {
       <div className={styles.background}>
           <div className={styles.content}>
             
-            <Image src="/pp.png" alt="Photo de profil" width={150} height={150}  style={{position: 'absolute',
+            <Image src="/pp2.png" alt="Photo de profil" width={150} height={150}  style={{position: 'absolute',
               left: '50%',
               bottom: '0%',
               top: '0%',
               transform: 'translate(-50%, -50%)',
+              border: '1px solid transparent',
+              borderRadius: '50%',
+              boxShadow: 'inset 0 0 0 15px black',
               zIndex: '3',}}>
             </Image>
             
             <h1 className={styles.title}>Wujciak Florentin</h1>
             <h2 className={styles.subtitle}>Junior App Developer</h2>
             
-            <div className={styles.divider}></div>
+            <div className={styles.dividerHorizontal}></div>
             
             <div className={styles.iconBox}>
               <div className={styles.iconItem}>
@@ -37,15 +40,47 @@ export default function Home() {
               </div>
             </div>
 
-            <div className={styles.divider}></div>
+            <div className={styles.dividerHorizontal}></div>
 
-            <div className={styles.aboutMe}>
-              <div className={styles.aboutMeIcon}>
-                <svg xmlns="http://www.w3.org/2000/svg" style={{marginLeft: "11px"}} height="16" width="14" viewBox="0 0 448 512"><path fill="#ffffff" d="M304 128a80 80 0 1 0 -160 0 80 80 0 1 0 160 0zM96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM49.3 464H398.7c-8.9-63.3-63.3-112-129-112H178.3c-65.7 0-120.1 48.7-129 112zM0 482.3C0 383.8 79.8 304 178.3 304h91.4C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7H29.7C13.3 512 0 498.7 0 482.3z"/></svg>
+            <div className={styles.dividerVertical}>
+              <div className={styles.leftPart}>
+
+                <div className={styles.aboutMeTitle}>
+                  <div className={styles.aboutMeIcon}>
+                    <svg xmlns="http://www.w3.org/2000/svg" style={{marginLeft: "11px"}} height="16" width="14" viewBox="0 0 448 512"><path fill="#ffffff" d="M304 128a80 80 0 1 0 -160 0 80 80 0 1 0 160 0zM96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM49.3 464H398.7c-8.9-63.3-63.3-112-129-112H178.3c-65.7 0-120.1 48.7-129 112zM0 482.3C0 383.8 79.8 304 178.3 304h91.4C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7H29.7C13.3 512 0 498.7 0 482.3z"/></svg>
+                  </div>
+                  <h3>About Me</h3>
+                </div>
+
+                <br></br>
+
+                <div className={styles.aboutMeText}>
+                  <p>Passionné par l'informatique, je suis actuellement en troisième année de BUT Informatique, et suis actuellement en recherche de stage. Je possède des compétence en Java, Python, PHP, et je me forme actuellement en autodidacte en C#. Je souhaiterai plus tard fonder mon entreprise, lorsque j'aurais acquis assez d'expérience en tant que développeur.</p>
+                </div>
+
+                <div className={styles.dividerHorizontal}></div>
+              
               </div>
-              <h3>About Me</h3>
+
+              <div className={styles.middlePart}></div>
+
+              <div className={styles.rightPart}>
+
+              <div className={styles.aboutMeTitle}>
+                  <div className={styles.aboutMeIcon}>
+                    <svg xmlns="http://www.w3.org/2000/svg" style={{marginLeft: "11px"}} height="16" width="14" viewBox="0 0 448 512"><path fill="#ffffff" d="M304 128a80 80 0 1 0 -160 0 80 80 0 1 0 160 0zM96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM49.3 464H398.7c-8.9-63.3-63.3-112-129-112H178.3c-65.7 0-120.1 48.7-129 112zM0 482.3C0 383.8 79.8 304 178.3 304h91.4C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7H29.7C13.3 512 0 498.7 0 482.3z"/></svg>
+                  </div>
+                  <h3>Compétences</h3>
+                </div>
+
+                <br></br>
+
+                <div className={styles.aboutMeText}>
+                  <p>Passionné par l'informatique, je suis actuellement en troisième année de BUT Informatique, et suis actuellement en recherche de stage. Je possède des compétence en Java, Python, PHP, et je me forme actuellement en autodidacte en C#. Je souhaiterai plus tard fonder mon entreprise, lorsque j'aurais acquis assez d'expérience en tant que développeur.</p>
+                </div>
+                
+              </div>
             </div>
-          
           </div>
       </div>
     </body>
